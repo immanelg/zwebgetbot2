576a5ce78f5ca4fcc72d8fd44d24346b67ea2aba
immanelg <119798691+immanelg@users.noreply.github.com>
2026-01-31 22:38:59 +0300

    fancy edit messages

--- main.js
+++ main.js
@@ -1,30 +1,38 @@
+#!/usr/bin/env node
 require('dotenv').config()
 
 const TelegramBot = require('node-telegram-bot-api');
 
 const spawn = require("child_process").spawn;
+const fs = require("fs");
 
 const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/(https?:\/\/)?(.*)/, (msg, match) => {
+bot.onText(/(https?:\/\/)?(.*)/, async (msg, match) => {
+    const chatId = msg.chat.id;
+
+    const botMsg = await bot.sendMessage(chatId, "Hello...");
+
     const scheme = match[1] ?? "https://";
     const url = scheme+match[2];
 
-    const chatId = msg.chat.id;
+    console.log("request", msg.from.id, url);
 
-    const filename = chatId.toString()+".html";
+    await bot.editMessageText("⏳ Downloading "+url, {chat_id: chatId, message_id: botMsg.message_id});
 
-    const child = spawn("single-file", [url, filename]);
-    child.on("close", exitCode => {
-        console.log(exitCode);
-        bot.sendDocument(chatId, filename, { contentType: 'text/html' });
+    const fsFilename = chatId.toString()+".html";
+
+    const stderrChunks = [];
+    const child = spawn("single-file", [url, fsFilename]);
+    child.on("close", async exitCode => {
+        console.log("single-file exited:", url, fsFilename, exitCode);
+        if (exitCode !== 0)
+            await bot.editMessageText(`❌ Error! Exit code ${exitCode}, stderr: ${""}`, {chat_id: chatId, message_id: botMsg.message_id});
+        else
+            await bot.sendDocument(chatId, fsFilename, {}, { contentType: 'text/html', filename: "index.html" });
+        fs.unlink(fsFilename, err => console.error(err));
     });
 });
 
-// bot.on('message', (msg) => {
-//     const chatId = msg.chat.id;
-// 
-//     bot.sendMessage(chatId, 'Received your message');
-// });
